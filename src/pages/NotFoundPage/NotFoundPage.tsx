1dcee031b44d7dbd24ffd4926717e6b395b22dce
Vladislav Chervyakov <chervyakovve@mail.ru>
2023-10-08 15:00:28 +0700

    feat: not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import classNames from 'classnames';
+import styles from './style.module.scss';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '@/shared/ui';
 
 export const NotFoundPage = () => {
-  return <section>NotFoundPage</section>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <section className={classNames('container', styles.page)}>
+      Тут ничего нет <Button onClick={handleClick}>На главную</Button>
+    </section>
+  );
 };
